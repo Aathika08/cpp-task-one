6998e4ca72964d9c028026b90784d5809ef4f4c2
aathika <83359584+Aathika08@users.noreply.github.com>
2021-11-27 21:16:31 +0530

    Add files via upload

---
+++ main (3).cpp
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"size of char:"<<sizeof(char)<<"byte"<<endl;
+    cout<<"size of int:"<<sizeof(int)<<"bytes"<<endl;
+    cout<<"size of float:"<<sizeof(float)<<"bytes"<<endl;
+    cout<<"size of double:"<<sizeof(double)<<"bytes"<<endl;
+    return 0;
+}
